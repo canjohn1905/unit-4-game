423336f6c7f61197d11febdc959ab70ae957a39d
John Atay <johncanaty@gmail.com>
2019-05-29 21:03:07 -0700

    updatefinal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
 // There will be four crystals displayed as buttons on the page.
 // The player will be shown a random number at the start of the game.
-// var targetScore = Math.floor((Math.random() * 120) + 19);
+//var targetScore = Math.floor((Math.random() * 120) + 19);
 var targetScore = 20
 var totalScore = 0;
+var wins=0
+var losses=0
 $("#targetScore").text(targetScore)
 $("#yourScore").text(totalScore)
+$(".winscore").text("wins: " + wins)
+$(".losescore").text("losses: " + wins)
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
 $(".diamond").on("click", function () {
@@ -17,12 +21,19 @@ $(".diamond").on("click", function () {
 
     if ( targetScore === totalScore) {
         alert("WIN");
-        //Reset the game (later)
+        totalScore=0;
+        $("#yourScore").text(totalScore);
+        wins++;
+        console.log(wins);
+        $(".winscore").text("wins: "+ wins);
     }
 
     if (totalScore > targetScore) {
-        alert ("LOSE")
+        alert ("LOSE");
+        losses++;
+        $(".losescore").text("losses: " +losses);
     }
+
 });
 
 
